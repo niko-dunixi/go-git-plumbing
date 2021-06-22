13c227198e55b6ab70d7344c5214c5b4293a944e
Michael Russell <michael.j.russell.email@gmail.com>
2021-06-21 20:45:42 -0700

    Added Dumb Test
    - dumb test for mustgitprojectrootdirectory heh
    
    Resolves: tests/small-util-test-edit

--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -17,3 +17,11 @@ func TestIndexOfStringInSliceUnhappy(t *testing.T) {
 		t.Errorf("wait wut...we actually found some cheeseburgers...something wrong in all the right ways...")
 	}
 }
+
+func TestMustGitProjectRootDirectoryDumbHappyTest(t *testing.T) {
+	result := MustGitProjectRootDirectory()
+	if len(result) == 0 {
+		t.Errorf("uh oh, guess we're not in a project root running test, or something broke?" +
+			" -could get crafty and ensure that where this file was, we always navigated to this directory to check _shrug_ hehh")
+	}
+}
