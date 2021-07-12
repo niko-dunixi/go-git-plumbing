fc3b45a87039c00f086af3f656a42a237b2b1a5f
Paul FREAKN Baker <paul.nelson.baker@gmail.com>
2021-07-12 15:01:51 -0600

    Improving Debug Output
    Git utilizes GIT_TRACE for output messaging, but this will need some
    adjusting to get the formatting to match. I don't know if we can, but
    GIT itself includes the filename and fileline name. I'm not sure if we
    can do that.

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/paul-nelson-baker/go-git-plumbing
 
 go 1.16
 
-require github.com/jessevdk/go-flags v1.5.0
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- internal/utils/utils_test.go
+++ internal/utils/utils_test.go
@@ -1,6 +1,11 @@
 package utils
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestIndexOfStringInSliceHappy(t *testing.T) {
 	setupStrArry := []string{"hello", "is", "it", "me", "you're", "looking", "for"}
@@ -23,5 +28,69 @@ func TestMustGitProjectRootDirectoryDumbHappyTest(t *testing.T) {
 	if len(result) == 0 {
 		t.Errorf("uh oh, guess we're not in a project root running test, or something broke?" +
 			" -could get crafty and ensure that where this file was, we always navigated to this directory to check _shrug_ hehh")
+	}
+}
+
+func TestShouldTrace(t *testing.T) {
+	strptr := func(s string) *string {
+		return &s
+	}
+	testcases := []struct {
+		environmentValue *string
+		expectedValue    bool
+	}{
+		{
+			environmentValue: nil,
+			expectedValue:    false,
+		},
+		{
+			environmentValue: strptr(""),
+			expectedValue:    false,
+		},
+		{
+			environmentValue: strptr("false"),
+			expectedValue:    false,
+		},
+		{
+			environmentValue: strptr("true"),
+			expectedValue:    true,
+		},
+		{
+			environmentValue: strptr("TRUE"),
+			expectedValue:    true,
+		},
+		{
+			environmentValue: strptr("1"),
+			expectedValue:    true,
+		},
+		{
+			environmentValue: strptr("2"),
+			expectedValue:    true,
+		},
+		{
+			environmentValue: strptr("3"),
+			expectedValue:    false,
+		},
+		{
+			environmentValue: strptr("0"),
+			expectedValue:    false,
+		},
+	}
+
+	defer func() {
+		os.Unsetenv("GIT_TRACE")
+	}()
+
+	for _, currentTestcase := range testcases {
+		os.Unsetenv("GIT_TRACE")
+		inputValue := "<nil>"
+		if environmentValue := currentTestcase.environmentValue; environmentValue != nil {
+			inputValue = *environmentValue
+			os.Setenv("GIT_TRACE", *environmentValue)
+		}
+		actualTraceValue := ShouldTrace()
+		assert.Equal(t, actualTraceValue, currentTestcase.expectedValue,
+			"input %s should have resulted in %t but was %t", inputValue, currentTestcase.expectedValue, actualTraceValue,
+		)
 	}
 }
